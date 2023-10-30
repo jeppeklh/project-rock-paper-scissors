61113b270ea30d533e07b81e436dd6d3acc7d85a
jeppeklh <jeppeklh@hotmail.com>
2023-10-30 21:47:09 +0100

    Added the game function
    The function plays 5 rounds. If it's a draw, no one gets a point

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerPoints = 0;
+let pcPoints = 0;
+
 function getComputerChoice() {
   const x = Math.floor(Math.random() * 3) + 1;
   let choice = "";
@@ -15,9 +18,11 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
+      pcPoints++;
       return "You Lose! Paper beats Rock";
     } else if (computerSelection == "scissors") {
-      return "You Win! Scissors beats Rock";
+      return "You Win! Rock beats Scissors";
+      playPoints++;
     } else {
       return "Draw!";
     }
@@ -25,8 +30,10 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === "paper") {
     if (computerSelection === "scissors") {
+      pcPoints++;
       return "You Lose! Scissors beats Paper";
     } else if (computerSelection == "rock") {
+      playerPoints++;
       return "You Win! Paper beats Rock";
     } else {
       return "Draw!";
@@ -35,8 +42,10 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
+      pcPoints++;
       return "You Lose! Rock beats Scissors";
     } else if (computerSelection == "paper") {
+      playerPoints++;
       return "You Win! Scissors beats Paper";
     } else {
       return "Draw!";
@@ -44,9 +53,25 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+function game() {
+  playerPoints = 0;
+  pcPoints = 0;
+  let roundNumber = 1;
 
+  for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt("Round: " + roundNumber + " Rock, Paper, Scissors?");
+    let pcChoice = getComputerChoice();
+    console.log("Round: " + roundNumber);
+    console.log("Player: " + playerChoice + " vs " + "PC: " + pcChoice);
+    console.log(playRound(playerChoice, pcChoice));
+    console.log("");
+
+    roundNumber++;
+  }
+
+  console.log("Player: " + playerPoints + " PC: " + pcPoints);
+}
 
 // const playerSelection = "rock";
 // const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
-
